2faaa7859166e238206501d32bc87ac10622c20a
erincodes <erinleeannscott@gmail.com>
2018-01-15 19:23:33 -0700

    Added documentation details for Milestone 1 of PWP.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PWP Milestone 1</title>
+		<!--<link rel="stylesheet" href="./style.css"/>
+		<script src="./script.js"></script>-->
+	</head>
+	<body>
+		<!--<img src="images/erin-scott.jpg" alt="Erin Scott">-->
+		<h1>Purpose:</h1>
+			<p>I would like my PWP to serve as a portfolio of my past work and enable visitors to easily get in contact with me.</p>
+		<h1>Audience:</h1>
+			<p>My target audience with this portfolio website is future freelance clients.</p>
+		<h1>Goal:</h1>
+			<p>The primary goal of this website is to generate leads from potential freelance clients after I graduate from bootcamp. </p>
+		<h1>Persona:</h1>
+		<p>An example persona of someone in my primary target audience is:</p>
+				<ul>
+					<li>Debi is 60 years old and is transitioning towards retiring from her job as a paralegal. As a way to make money in retirement, Debi is opening a consulting business to offer her paralegal expertise to law firms on a part time, contract basis. Debi is comfortable using her PC laptop and iPhone, but doesn't consider herself particularly techincally savvy. She feels overwhelmed at the prospect of creating a website for her new business on her own. Debi's goal of creating a website is have a landing page future clients of her own to get in touch with her. She would prefer to work with someone to make updates to her website, versus having to learn and manage her site on her own.</li>
+					<li>Debi finds out about Erin's ability to make websites at a networking event, and goes to Erin's portfolio to see some examples of her past work. She decides that she wants to talk further with Erin about hiring her to work on a website for her new consulting business. Debi submits her information via the contact form on Erin's site requesting a quote for the project.</li>
+				</ul>
+	</body>
+</html>
