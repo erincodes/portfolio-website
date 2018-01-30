30828574fa205c4fbc379098a8093998b8642b97
erincodes <erinleeannscott@gmail.com>
2018-01-29 22:03:29 -0700

    Made a few tweaks to the content sections, added "testimonials".

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,19 +6,19 @@
 	</head>
 	<body>
 		<h1>PWP Milestone 2</h1>
-		<h2>Content Strategy</h2>
-			<p>Document - in detail - the content you plan to feature in each section of your site. Be sure to include any details regarding interactive features and functionality that you wish to include. You are not expected to have any actual content prepared at this time, rather only a plan for the kind of content you would like to feature.</p>
-			<p>Keep this section brief and succinct, yet detailed enough to guide your development phase. One paragraph per content section should be sufficient. Use the Sample PWP Milestones as a guide.</p>
+		<h2><em>Content Strategy</em></h2>
 			<h3>Introduction:</h3>
-				<p>The introduction section of the content will be the first thing the user sees. I'd like to be clean and simple: my name, tagline, and some sort of office/desk photo that embodies an "organized" vibe. I'm toying with the idea of including a small professional photo of myself somewhere in the introduction as well.</p>
+				<p>The introduction section of the content will be the first thing the user sees. I'd like to be clean and simple: my name, tagline, and some sort of office/desk photo that embodies an "organized" vibe. I'm toying with the idea of including a small professional photo of myself somewhere in the introduction as well. Below the "hero" image, I'd like to include a horizontal menu with the content sections I have mapped out below, and have them serve as anchor links to jump down the page. This will condense to a hamburger menu on mobile.</p>
 			<h3>Portfolio:</h3>
 				<p>I'd like the portfolio section to be a grid setup that feature 3 past projects, with the potential to add additional rows as my portfolio of work increases. When users hover over each project, I'd like to implement some sort of interactive functionality so that a "more details" bubble pops up and describes my contributions to the project/technology used/etc.</p>
+			<h3>Testimonials:</h3>
+				<p>This section will include quotes and names from people who recommend working me. I'd like to interactively rotate through numerous quotes.</p>
 			<h3>Contact:</h3>
-				<p>This section will include a contact form and my email address.</p>
+				<p>This section will be the "call to action". It will include a contact form and my professional email address. I'd like to include a "check box" opt-in section to subscribe to my email list, as a long-term goal of mine is to build a blog.</p>
 			<h3>Footer:</h3>
 				<p>I'd like to have a footer that includes social media icons that link to my LinkedIn, Twitter, and GitHub pages.</p>
 				<br>
-		<h2>Wireframes</h2>
+		<h2><em>Wireframes</em></h2>
 		<h3>Mobile Wireframe</h3>
 			<p>Place your wireframe images in your milestone-2.php document. Image files must be less than 2MB in size, and in SVG, JPEG, PNG, or GIF format.</p>
 		<h3>Desktop Wireframe</h3>
