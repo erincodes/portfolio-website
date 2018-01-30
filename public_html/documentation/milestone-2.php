cf9f17a131ee1dcbf601504173b3d9d5f9b077ce
erincodes <erinleeannscott@gmail.com>
2018-01-29 21:48:14 -0700

    Added first draft of site content plan.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PWP Milestone 2</title>
+	</head>
+	<body>
+		<h1>PWP Milestone 2</h1>
+		<h2>Content Strategy</h2>
+			<p>Document - in detail - the content you plan to feature in each section of your site. Be sure to include any details regarding interactive features and functionality that you wish to include. You are not expected to have any actual content prepared at this time, rather only a plan for the kind of content you would like to feature.</p>
+			<p>Keep this section brief and succinct, yet detailed enough to guide your development phase. One paragraph per content section should be sufficient. Use the Sample PWP Milestones as a guide.</p>
+			<h3>Introduction:</h3>
+				<p>The introduction section of the content will be the first thing the user sees. I'd like to be clean and simple: my name, tagline, and some sort of office/desk photo that embodies an "organized" vibe. I'm toying with the idea of including a small professional photo of myself somewhere in the introduction as well.</p>
+			<h3>Portfolio:</h3>
+				<p>I'd like the portfolio section to be a grid setup that feature 3 past projects, with the potential to add additional rows as my portfolio of work increases. When users hover over each project, I'd like to implement some sort of interactive functionality so that a "more details" bubble pops up and describes my contributions to the project/technology used/etc.</p>
+			<h3>Contact:</h3>
+				<p>This section will include a contact form and my email address.</p>
+			<h3>Footer:</h3>
+				<p>I'd like to have a footer that includes social media icons that link to my LinkedIn, Twitter, and GitHub pages.</p>
+				<br>
+		<h2>Wireframes</h2>
+		<h3>Mobile Wireframe</h3>
+			<p>Place your wireframe images in your milestone-2.php document. Image files must be less than 2MB in size, and in SVG, JPEG, PNG, or GIF format.</p>
+		<h3>Desktop Wireframe</h3>
+	</body>
+</html>
