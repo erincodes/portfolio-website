3886618cb3e2f4d552eefd0c0e610a38fd0dc8f5
erincodes <erinleeannscott@gmail.com>
2018-01-31 13:01:41 -0700

    Uploaded desktop wireframe image and placed in content, did a final content review before submitting to instructors.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,13 +8,13 @@
 		<h1>PWP Milestone 2</h1>
 		<h2><em>Content Strategy</em></h2>
 			<h3>Header:</h3>
-				<p>The header section of the content will serve as a landing page and will be the first thing the user sees. I'd like it to be clean and simple: my name, tagline, and some sort of office/desk photo that embodies an "organized" vibe. Above the "hero" image, I'd like to include a horizontal menu bar with the content sections I have mapped out below, and have them serve as anchor links to jump down the page. This will condense to a hamburger menu on mobile.</p>
+				<p>The header section of the content will serve as a landing page and will be the first thing the user sees. I'd like it to be clean and simple: my name, tagline, and some sort of office/desk photo that embodies an "organized" vibe. Above the "hero" image, I'd like to include a horizontal menu bar with the content sections I have mapped out below, and have them serve as anchor links to jump down the page. This menu will condense to a hamburger menu on mobile.</p>
 			<h3>About Me:</h3>
-				<p>This section will serve as a personal introduction. I'd like to include a professional photo of myself and 2-3 short lines of text that encapsulates my elevator pitch.</p>
+				<p>This section will serve as a personal introduction. I'd like to include a professional photo of myself and 2-3 short lines of text that encapsulate my elevator pitch.</p>
 			<h3>Testimonials:</h3>
-				<p>This section will include quotes and names from people who recommend working with me. I'd like to interactively rotate through numerous quotes.</p>
+				<p>This section will include quotes and names from people who recommend working with me. I'd like to have the quotes interactively rotate.</p>
 			<h3>Portfolio:</h3>
-				<p>I'd like the portfolio section to be a grid setup that features 2-3 past projects, with the potential to add additional rows as my portfolio of work increases. When users hover over each project, I'd like to implement some sort of interactive functionality so that a "more details" bubble pops up and describes my contributions to the project/technology used/etc.</p>
+				<p>I'd like the portfolio section to be a grid setup that features 2-3 past projects, with the potential to add additional rows as my portfolio of work increases. When users hover over each project, I'd like to implement some sort of interactive functionality so that a "more details" bubble pops up and describes my contributions to the project/technology used/etc. This section will have a "swipe for more" functionality.</p>
 			<h3>Contact:</h3>
 				<p>This section will be the "call to action." It will include a contact form and my professional email address. I'd like to include a "check box" opt-in section to subscribe to my email list, as a long-term goal of mine is to build a blog and distribute posts to email subscribers.</p>
 			<h3>Footer:</h3>
@@ -25,6 +25,7 @@
 			<img src="images/erin-wireframe-mobile.png" alt="Mobile Wireframe">
 	  	<br>
 		<h3>Desktop Wireframe</h3>
+			<img src="images/erin-wireframe-desktop.png" alt="Desktop Wireframe">
 		<br>
 	</body>
 </html>
