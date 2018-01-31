1b5a007ae5f8c2e6d1c7ae4d88842539f355aedb
erincodes <erinleeannscott@gmail.com>
2018-01-30 20:03:52 -0700

    Revised content sections/spell check.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,16 +7,18 @@
 	<body>
 		<h1>PWP Milestone 2</h1>
 		<h2><em>Content Strategy</em></h2>
-			<h3>Introduction:</h3>
-				<p>The introduction section of the content will be the first thing the user sees. I'd like to be clean and simple: my name, tagline, and some sort of office/desk photo that embodies an "organized" vibe. I'm toying with the idea of including a small professional photo of myself somewhere in the introduction as well. Below the "hero" image, I'd like to include a horizontal menu with the content sections I have mapped out below, and have them serve as anchor links to jump down the page. This will condense to a hamburger menu on mobile.</p>
-			<h3>Portfolio:</h3>
-				<p>I'd like the portfolio section to be a grid setup that feature 3 past projects, with the potential to add additional rows as my portfolio of work increases. When users hover over each project, I'd like to implement some sort of interactive functionality so that a "more details" bubble pops up and describes my contributions to the project/technology used/etc.</p>
+			<h3>Header:</h3>
+				<p>The header section of the content will serve as a landing page and will be the first thing the user sees. I'd like it to be clean and simple: my name, tagline, and some sort of office/desk photo that embodies an "organized" vibe. Above the "hero" image, I'd like to include a horizontal menu bar with the content sections I have mapped out below, and have them serve as anchor links to jump down the page. This will condense to a hamburger menu on mobile.</p>
+			<h3>About Me:</h3>
+				<p>This section will serve as a personal introduction. I'd like to include a professional photo of myself and 2-3 short lines of text that encapsulates my elevator pitch.</p>
 			<h3>Testimonials:</h3>
-				<p>This section will include quotes and names from people who recommend working me. I'd like to interactively rotate through numerous quotes.</p>
+				<p>This section will include quotes and names from people who recommend working with me. I'd like to interactively rotate through numerous quotes.</p>
+			<h3>Portfolio:</h3>
+				<p>I'd like the portfolio section to be a grid setup that features 2-3 past projects, with the potential to add additional rows as my portfolio of work increases. When users hover over each project, I'd like to implement some sort of interactive functionality so that a "more details" bubble pops up and describes my contributions to the project/technology used/etc.</p>
 			<h3>Contact:</h3>
 				<p>This section will be the "call to action." It will include a contact form and my professional email address. I'd like to include a "check box" opt-in section to subscribe to my email list, as a long-term goal of mine is to build a blog and distribute posts to email subscribers.</p>
 			<h3>Footer:</h3>
-				<p>I'd like to have a footer that includes social media icons that link to my LinkedIn, Twitter, and GitHub pages. Ideally this would be included on any and all future pages on the site.</p>
+				<p>I'd like to have a footer that includes social media icons that link to my GitHub, LinkedIn, and Twitter pages. Ideally this would be "sticky" and would appear as the footer on each section when viewing the site on a mobile device.</p>
 		<br>
 		<h2><em>Wireframes</em></h2>
 		<h3>Mobile Wireframe</h3>
