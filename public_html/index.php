0e436e65fc114b3f1df486bf7fb3d50f3fff2a35
erincodes <erinleeannscott@gmail.com>
2018-02-11 21:12:00 -0700

    Added jumbotron, sticky footer, and templated out sections according to wireframes.

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,4 @@
+<!--Refer to content strategy here: https://bootcamp-coders.cnm.edu/~escott15/portfolio-website/public_html/documentation/milestone-2.php-->
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -22,5 +23,55 @@
 		<!--Example element formats if needed:-->
 		<!--<img src="images/erin-scott.jpg" alt="Erin Scott">-->
 		<!--<a href="https://www.w3schools.com/" target="_blank">Visit W3Schools!</a>-->
+		<header>
+			<nav class="navbar">
+			</nav>
+		</header>
+		<section>
+			<div class="container-fluid">
+				<div class="row-fluid">
+					<div class="column">
+						<div class="jumbotron jumbotron-fluid">
+							<div class="container">
+								<h1 class="display-3 text-center">Erin Scott</h1>
+								<p class="lead text-center">Web Development, Marketing, and Project Management</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="column">
+
+					</div>
+				</div>
+			</div>
+		</section>
+		<section><div class="container">
+				<div class="row">
+					<div class="column">
+
+					</div>
+				</div>
+			</div>
+		</section>
+		<section><div class="container">
+				<div class="row">
+					<div class="column">
+
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer>
+			<div class="container-fluid">
+				<div class="sticky-footer">
+					<p class="text-center text-white bg-dark p-4 m-0 fixed-bottom">Erin Scott ©2018</p>
+				</div>
+			</div>
+		</footer>
 	</body>
 </html>
