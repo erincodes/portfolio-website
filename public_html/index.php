b6b3bf46e5c53b3b3aba8ad975b43ac609eb20e9
erincodes <erinleeannscott@gmail.com>
2018-02-21 21:44:55 -0700

    Added rough form template

--- public_html/index.php
+++ public_html/index.php
@@ -153,7 +153,46 @@
 					<div class="column mt-5 pb-5 mb-5">
 						<h2>Contact</h2>
 						<p class="mx-auto">erinleeannscott@gmail.com</p>
-						<p>Pellentesque hendrerit condimentum mi ut convallis. Suspendisse volutpat nisl vel suscipit viverra. Nunc sit amet turpis vitae turpis lobortis dignissim. Suspendisse eros mi, faucibus id massa eu, gravida suscipit felis. Curabitur suscipit sapien sit amet ex rhoncus dictum. Donec in lorem blandit, dignissim justo at, tempor ex. Praesent nec eros dignissim, pulvinar nisl eget, ornare nibh. Ut sagittis facilisis enim a feugiat. Proin eget elit ligula.</p>
+						<form class="form-horizontal well" action="email.php">
+							<div class="form-group">
+								<label for="name">Name</label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-user" aria-hidden="true"></i>
+									</div>
+									<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email">Email address</label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-envelope" aria-hidden="true"></i>
+									</div>
+									<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="subject">Subject</label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-pencil" aria-hidden="true"></i>
+									</div>
+									<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="message">Message</label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-comment" aria-hidden="true"></i>
+									</div>
+									<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message"></textarea>
+								</div>
+							</div>
+							<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+							<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+						</form>
 					</div>
 				</div>
 			</div>
